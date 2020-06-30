5d5b64706d7a9b2fb26abcf6d0aca337d4fb7fb9
Jaroslav Holub <jaroslav@automato.io>
2020-06-30 14:28:10 +0200

    add incident ls unfinished table output

--- cmd/incident.go
+++ cmd/incident.go
@@ -1,13 +1,24 @@
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
+	"strconv"
 
+	util "github.com/automato-io/binocs-cli/util"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
 
 // Incident comes from the API as a JSON
 type Incident struct {
+	ID            int    `json:"id"`
+	CheckID       int    `json:"check_id"`
+	IncidentNote  string `json:"incident_note"`
+	IncidentState string `json:"incident_state"`
 }
 
 func init() {
@@ -21,7 +32,45 @@ var incidentCmd = &cobra.Command{
 	Long:    `...`,
 	Aliases: []string{"incidents"},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("display incidents")
+		respData, err := util.BinocsAPI("/incidents", http.MethodGet, []byte{})
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		respJSON := make([]Incident, 0)
+		decoder := json.NewDecoder(bytes.NewBuffer(respData))
+		err = decoder.Decode(&respJSON)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		var tableData [][]string
+		for _, v := range respJSON {
+			checkData, err := util.BinocsAPI("/checks/"+strconv.Itoa(v.CheckID), http.MethodGet, []byte{})
+			if err != nil {
+				// skip non-existent or deleted checks and continue
+				continue
+			}
+			var check Check
+			err = json.Unmarshal(checkData, &check)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			// @todo this URL is not necessarily the one that caused the incident
+			tableRow := []string{
+				strconv.Itoa(v.ID), check.Name, check.URL, v.IncidentState, "2010", "2020", "500, 503", v.IncidentNote,
+			}
+			tableData = append(tableData, tableRow)
+		}
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"ID", "CHECK", "URL", "STATE", "OPENED", "CLOSED", "RESPONSE CODES", "NOTE"})
+		for _, v := range tableData {
+			table.Append(v)
+		}
+		table.Render()
 	},
 }
 
