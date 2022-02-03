12e33e4ceebfd7ffd7a7284681b80a45e4fbd521
Jaroslav Holub <jaroslav@automato.io>
2022-02-03 11:34:48 +0700

    fix default config path in documentation

--- cmd/root.go
+++ cmd/root.go
@@ -100,7 +100,7 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.binocs-cli.json)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.binocs/config.json)")
 	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
 
 	// Cobra also supports local flags, which will only run
