2ae9112fd51a87255993fe0b8968c76eedf73d50
Jaroslav Holub <jaroslav@automato.io>
2022-03-04 16:01:29 +0700

    add regions command

---
+++ cmd/regions.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(regionsCmd)
+}
+
+var regionsCmd = &cobra.Command{
+	Use:   "regions",
+	Short: "List supported regions",
+	Long: `
+List the regions Binocs makes requests from
+`,
+	DisableAutoGenTag: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		spin.Start()
+		spin.Suffix = " loading regions..."
+
+		loadSupportedRegions()
+
+		var tableData [][]string
+		for _, v := range supportedRegions {
+			tableRow := []string{
+				v,
+			}
+			tableData = append(tableData, tableRow)
+		}
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetAutoWrapText(false)
+		table.SetHeader([]string{"REGION"})
+		for _, v := range tableData {
+			table.Append(v)
+		}
+		spin.Stop()
+		table.Render()
+	},
+}
