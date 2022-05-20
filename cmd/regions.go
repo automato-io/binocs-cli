58d12a073fc0eddfb821ec1c7a246c9ca7747b66
Jaroslav Holub <jaroslav@automato.io>
2022-05-20 12:00:12 +0700

    minor refactor

--- cmd/regions.go
+++ cmd/regions.go
@@ -34,8 +34,8 @@ List the regions Binocs makes requests from
 		table := tablewriter.NewWriter(os.Stdout)
 		table.SetAutoWrapText(false)
 		table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
-		table.SetHeader([]string{"REGIONS"})
 		table.SetHeaderColor(tablewriter.Colors{tablewriter.Bold})
+		table.SetHeader([]string{"REGIONS"})
 		for _, v := range tableData {
 			table.Append(v)
 		}
