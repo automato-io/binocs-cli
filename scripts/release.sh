c42efa937d424b670937fc11302c4f28696a1169
Jaroslav Holub <jaroslav@automato.io>
2022-11-05 14:02:29 +0100

    fix release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,8 +7,8 @@ BINOCS_VERSION="$1"
 
 pushd ..
 git add cmd/root.go
-git commit -m 'bump version to ${BINOCS_VERSION}'
-git tag -a v${BINOCS_VERSION} -m 'release v${BINOCS_VERSION}'
+git commit -m "bump version to ${BINOCS_VERSION}"
+git tag -a v${BINOCS_VERSION} -m "release v${BINOCS_VERSION}"
 git push origin master
 git push origin v${BINOCS_VERSION}
 popd
